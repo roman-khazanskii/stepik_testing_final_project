9a54a06625e9aadc57438ffc361038466885f2fb
roman-khazanskii <r.hazansky@spbstu.ru>
2020-06-11 01:56:20 +0300

    Добавил тесты для страницы логина/регистрации

--- pages/locators.py
+++ pages/locators.py
@@ -4,4 +4,6 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     
-#class LoginPageLocators():    
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -11,9 +11,7 @@ class LoginPage(BasePage):
         assert "login" in self.browser.current_url
         
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
