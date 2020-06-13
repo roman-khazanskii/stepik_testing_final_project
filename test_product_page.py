6db2903e34c945d8932d35ee9fb60ebecef784cc
roman-khazanskii <r.hazansky@spbstu.ru>
2020-06-13 20:30:37 +0300

    Отмаркировал непроходящие как xfail, ибо они и не должны проходить; пачку из 10 офферов пока скипнул

--- test_product_page.py
+++ test_product_page.py
@@ -1,25 +1,27 @@
 from .pages.product_page import ProductPage
 import pytest
 
-# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  # pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-# def test_guest_can_add_product_to_basket(browser, link):
-    # # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019."
-    # # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    # page = ProductPage(browser, link)   
-    # # открываем страницу
-    # page.open()                      
-    # # пытаемся добавить товар в корзину
-    # page.add_to_basket_promo()       
-    
+@pytest.mark.skip(reason="Too long")
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019."
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page = ProductPage(browser, link)   
+    # открываем страницу
+    page.open()                      
+    # пытаемся добавить товар в корзину
+    page.add_to_basket_promo()       
+
+@pytest.mark.xfail    
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):     
     # Открываем страницу товара 
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -29,7 +31,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.add_to_basket()
     # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
     page.check_success_message_not_present()
-    
+
 def test_guest_cant_see_success_message(browser): 
     #Открываем страницу товара 
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -37,7 +39,8 @@ def test_guest_cant_see_success_message(browser):
     page.open()                      
     #Проверяем, что нет сообщения об успехе с помощью is_not_element_present
     page.check_success_message_not_present()
-    
+
+@pytest.mark.xfail    
 def test_message_disappeared_after_adding_product_to_basket(browser): 
     # Открываем страницу товара
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
