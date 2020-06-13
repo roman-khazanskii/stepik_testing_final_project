001e47ef740edc3b439d7a04a22cb425b282bdc1
roman-khazanskii <r.hazansky@spbstu.ru>
2020-06-13 19:03:58 +0300

    Поправил взятие содержимого тегов; все прохождения до этого были ложноположительными :(

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,23 +9,24 @@ import time
 class ProductPage(BasePage):
     def add_to_basket(self):
         
-        expected_product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).get_attribute("value")
-        expected_product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).get_attribute("value")
+        expected_product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        expected_product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        
+        print(f"Exp Name = {expected_product_name}, exp price = {expected_product_price}")
         
         btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
         btn.click()
         
         # получаем код, которым доказываем, что мы не человек
         self.solve_quiz_and_get_code()
-        
+                
         # название товара в корзине должно совпадать с добавляемым
-        added_product_name = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_NAME).get_attribute("value")
+        added_product_name = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_NAME).text
         assert expected_product_name == added_product_name, "Added product is not the one that was supposed to be added!"
         
         # и полная цена корзины из одного товара должна совпадать с ценой товара
-        total_price = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_PRICE).get_attribute("value")
-        assert expected_product_price == added_product_name, "Total price is not equal to product price"
-        
+        total_price = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_PRICE).text
+        assert expected_product_price == total_price, "Total price is not equal to product price"
        
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
@@ -33,6 +34,7 @@ class ProductPage(BasePage):
         answer = str(math.log(abs((12 * math.sin(float(x))))))
         alert.send_keys(answer)
         alert.accept()
+        time.sleep(1)
         try:
             alert = self.browser.switch_to.alert
             alert_text = alert.text
